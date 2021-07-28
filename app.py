4c9323c0dbd37ac2911d40f4403a33010a631476
Croix <croix.suhy@gmail.com>
2021-07-28 18:48:44 -0400

    Changed app.py to not run in debug mode

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from os import getcwd
 # Get the location of the template
-app = Flask(__name__)  # , template_folder=getcwd())
+app = Flask(__name__, template_folder=getcwd())
 
 
 @app.route("/")
 def home():
     # Use the html file
-    return "Hello World!"
+    return render_template("test.html")
 
 
-app.run()
+app.run(debug=False)
