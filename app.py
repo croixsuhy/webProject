45df5a299902a8ad89771f2497303b3340bc208f
Croix <croix.suhy@gmail.com>
2021-07-29 23:32:48 -0400

    Added RuntimeError to app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ app = Flask(__name__, template_folder=getcwd())
 
 @app.route("/<cats>")
 def numOfCats(cats):
-    if int(cats) < 5:
-        return f"You have {cats} cats!"
-    else:
-        return f"You have {cats} cats! That's alot!"
+    try:
+        if int(cats) < 5:
+            return f"You have {cats} cats!"
+        elif int(cats) > 5:
+            return f"You have {cats} cats! That's alot!"
+    except RuntimeError:
+        print("Can not run for some reason")
