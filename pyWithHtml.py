3330395969a50b296f4b2167323f61839d51e11a
Croix <croix.suhy@gmail.com>
2021-08-10 13:09:05 -0500

    Changed list to a range

--- pyWithHtml.py
+++ pyWithHtml.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder=getcwd())
 @app.route("/")
 def list():
     # List of fruits
-    fruits = ["Apple", "Bannana", "Orange"]
+    fruits = list(range(10))
 
     # Render template and use list
     return render_template("listTest.html", title="Fruits", fruits=fruits)
