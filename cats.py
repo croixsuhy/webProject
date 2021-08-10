6b4aa35d66afd01adde94d185d1d2c4ff36eac9f
Croix <croix.suhy@gmail.com>
2021-08-10 13:25:25 -0500

    Added comments to cats.py

--- cats.py
+++ cats.py
@@ -4,16 +4,20 @@ from os import getcwd
 app = Flask(__name__, template_folder=getcwd())
 
 
+# /<cats> makes it so you can use an input in the search bar
 @app.route("/<cats>")
 def numOfCats(cats):
     try:
-        if int(cats) == 0:
+        if int(cats) == None:
+            return "<title>Enter a number in the search bar!</title>"
+        # Changes cats variable to an int
+        elif int(cats) == 0:
             return "You have no cats... how sad..."
         elif int(cats) == 1:
-            return "You have 1 cat!"
+            return "You have a cat! Go get some more!"
         elif int(cats) < 5:
             return f"You have {cats} cats!"
         elif int(cats) > 5:
             return f"You have {cats} cats! That's alot!"
     except RuntimeError:
-        print("Can not run for some reason")
+        print("Can not run")
