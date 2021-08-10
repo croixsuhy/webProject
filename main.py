c1ab43f5f234685c7ec66d049bd3428a8284b87f
Croix <croix.suhy@gmail.com>
2021-08-10 13:11:35 -0500

    Accidentally deleted main (added back main.py)

---
+++ main.py
@@ -0,0 +1,4 @@
+from pyWithHtml import app  # Make sure to change import when using new python code
+
+if __name__ == "__main__":
+    app.run()
--- pyWithHtml.py
+++ pyWithHtml.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder=getcwd())
 @app.route("/")
 def list():
     # List of fruits
-    fruits = list(range(10))
+    fruits = ["Apple", "Bannana", "Orange"]
 
     # Render template and use list
     return render_template("listTest.html", title="Fruits", fruits=fruits)
