db3bac2212c223ead8a9869d7286ae254f52f860
Croix <croix.suhy@gmail.com>
2021-08-10 13:29:05 -0500

    Changed RuntimeError message in cats.py

--- cats.py
+++ cats.py
@@ -8,16 +8,21 @@ app = Flask(__name__, template_folder=getcwd())
 @app.route("/<cats>")
 def numOfCats(cats):
     try:
-        if int(cats) == None:
-            return "<title>Enter a number in the search bar!</title>"
-        # Changes cats variable to an int
-        elif int(cats) == 0:
-            return "You have no cats... how sad..."
-        elif int(cats) == 1:
-            return "You have a cat! Go get some more!"
-        elif int(cats) < 5:
-            return f"You have {cats} cats!"
-        elif int(cats) > 5:
-            return f"You have {cats} cats! That's alot!"
+        try:
+            if int(cats) == None:
+                return "<title>Enter a number in the search bar!</title>"
+            # Changes cats variable to an int
+            elif int(cats) == 0:
+                return "You have no cats... how sad..."
+            elif int(cats) == 1:
+                return "You have a cat! Go get some more!"
+            elif int(cats) < 5:
+                return f"You have {cats} cats!"
+            elif int(cats) > 5:
+                return f"You have {cats} cats! That's alot!"
+
+        except:
+            return "Can not run (our cats are having issues running the site...)"
+
     except RuntimeError:
-        return "Can not run (our cats are having issues running the site...)"
+        print("Application can not run")
